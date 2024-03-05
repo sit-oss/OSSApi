c59dfa157ce62122bf019f894191515eea5209e0
TK <tk@ccc.xxx>
2024-03-05 10:18:47 +0900

    feat: add Authorization

---
+++ OSSApi/Controllers/MyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OSSApi.Controllers
+{
+    /// <summary>
+    /// User controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class MyController : ControllerBase
+    {
+        /// <summary>
+        /// Get the user claims
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("claims")]
+        public IActionResult GetClaims()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
--- OSSApi/Program.cs
+++ OSSApi/Program.cs
@@ -1,4 +1,8 @@
 
+using System.Reflection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 namespace OSSApi
 {
     public class Program
@@ -7,12 +11,68 @@ namespace OSSApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
+
+            var ucenterAddr = builder.Configuration["UserCenter"];
+            if (string.IsNullOrEmpty(ucenterAddr))
+            {
+                throw new Exception("UserCenter address is not configured");
+            }
+
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            builder.Services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = ucenterAddr;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false
+                    };
+                });
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiScope", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", "apidev");
+                });
+            });
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Values API",
+                    Description = "An ASP.NET Core Web API for managing Values items",
+                    // TermsOfService = new Uri("https://example.com/terms"),
+                    // Contact = new OpenApiContact
+                    // {
+                    //     Name = "Example Contact",
+                    //     Url = new Uri("https://example.com/contact")
+                    // },
+                    // License = new OpenApiLicense
+                    // {
+                    //     Name = "Example License",
+                    //     Url = new Uri("https://example.com/license")
+                    // }
+                });
+                options.AddSecurityDefinition(name: "Bearer", securityScheme: new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
 
             var app = builder.Build();
 
@@ -23,10 +83,12 @@ namespace OSSApi
                 app.UseSwaggerUI();
             }
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
-            app.MapControllers();
+            app.MapControllers()
+                .RequireAuthorization("ApiScope");
 
             app.Run();
         }
